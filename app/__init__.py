ca07f92cf1d6996edc10b6979b1773acafc82cfe
K <kaustubh.bhavsar1@gmail.com>
2026-02-23 20:18:54 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from .routes import main
 from .database import init_db
+import os
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'dev'
-    app.config['DATABASE'] = 'instance/scans.db'
+    app.config['DATABASE'] = os.path.join('/tmp', 'scans.db')
+
     init_db(app)
     app.register_blueprint(main)
     return app
